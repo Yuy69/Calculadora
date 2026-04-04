d2d3a2473f9d6387475666c13cec4a21ee0fdd34
Yuy <btv2990@gmail.com>
2026-04-04 16:54:01 -0300

    update
    Melhorei a expressão na linha 45, implementei estrutura de repetição e adicionei uma mensagem de advertência sobre as alternativas ao usuário.

--- main.py
+++ main.py
@@ -16,34 +16,42 @@ print("""
 ==================================
 """)
 
-opcoes = input("Qual operação você deseja?")
+while True:
 
-if opcoes == "1":
-    numero1 = float(input("Digite um número.."))         #Este bloco representa o parte responsável por fazer o cálculo de adição.
-    numero2 = float(input("digite outro número.."))
-    soma = numero1 + numero2
-    print(f"O resultado de sua operação foi:{soma:.2f}")
+    opcoes = input("Qual operação você deseja?")
 
-elif opcoes == "2":
-    numero1 = float(input("Digite um número.."))          #Este bloco representa a parte responsável por fazer o cálculo de subtração.
-    numero2 = float(input("Digite outro número.."))
-    calculo = numero1 - numero2
-    print(f"O resultado de sua operação foi: {calculo:.2f}")
+    if opcoes == "1":
+        numero1 = float(input("Digite um número.."))         #Este bloco representa o parte responsável por fazer o cálculo de adição.
+        numero2 = float(input("digite outro número.."))
+        calculo = numero1 + numero2
+        print(f"O resultado de sua operação foi:{calculo:.2f}")
+        continuar = input("")
 
-elif opcoes == "3":
-    numero1 = float(input("Digite um número.."))            #Este bloco representa a parte responsável por fazer o cálculo de multiplicação.
-    numero2 = float(input("Digite outro número.."))
-    calculo = numero1 * numero2
-    print(f"O resultado de sua operação foi: {calculo:.2f}")
+    elif opcoes == "2":
+        numero1 = float(input("Digite um número.."))          #Este bloco representa a parte responsável por fazer o cálculo de subtração.
+        numero2 = float(input("Digite outro número.."))
+        calculo = numero1 - numero2
+        print(f"O resultado de sua operação foi: {calculo:.2f}")
 
-elif opcoes == "4":
-    numero1 = float(input("Digite um número.."))
-    numero2 = float(input("Digite outro número.."))
-    if numero1 and numero2 == 0: #se numero1 ou numero2 for igual 0: 
-        print("Desculpe, zero não pode ser dividido")  #Este bloco representa a parte responsável por fazer o cálculo de divisão.  
-    
+    elif opcoes == "3":
+        numero1 = float(input("Digite um número.."))            #Este bloco representa a parte responsável por fazer o cálculo de multiplicação.
+        numero2 = float(input("Digite outro número.."))
+        calculo = numero1 * numero2
+        print(f"O resultado de sua operação foi: {calculo:.2f}")
+
+    elif opcoes == "4":
+        numero1 = float(input("Digite um número.."))
+        numero2 = float(input("Digite outro número.."))
+        if numero2 == 0:      #se (número2) for igual a zero da erro pois zero nao pode ser dividido
+          print("Desculpe, zero não pode ser dividido")  #Este bloco representa a parte responsável por fazer o cálculo de divisão.  
+        
+        else:
+            calculo = numero1 / numero2
+            print(f"O resultado de sua operação foi: {calculo:.2f}")
     else:
-        calculo = numero1 / numero2
-        print(f"O resultado de sua operação foi: {calculo:.2f}")  
+        print(f"""A opção: ({opcoes}) Não existe, digite uma opção valida dentre:
+    [1]
+    [2]
+    [3]""")    #Este else tem a função de mandar uma mensagem para o usuário de que ele não escolheu nenhuma opção que está disponível
 
-      
+        
